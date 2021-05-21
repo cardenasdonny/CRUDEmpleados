7b63bd6e78c163b6bf3fff0d45d62cb9be915da5
cardenasdonny <54505199+cardenasdonny@users.noreply.github.com>
2021-05-21 13:52:41 -0500

    se edita usuario.cs

--- CRUDEmpleados/Models/Entities/Usuario.cs
+++ CRUDEmpleados/Models/Entities/Usuario.cs
@@ -7,5 +7,8 @@ namespace CRUDEmpleados.Models.Entities
 {
     public class Usuario
     {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
     }
 }
