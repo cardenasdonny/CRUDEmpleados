4f044696d4408739617272837051d98d7b3b45d4
Dony <cardenasdonny@gmail.com>
2021-05-21 13:42:55 -0500

    se crea la entidad Usuario.cs

---
+++ CRUDEmpleados/Models/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDEmpleados.Models.Entities
+{
+    public class Usuario
+    {
+    }
+}
